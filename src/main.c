c3b9824f221471d372f1efa6a56c86345a0a86b5
Grégoire Sage <gregoire.sage@gmail.com>
2016-02-21 23:38:05 +0100

    Change Enamel api

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 static Window *window;
 static TextLayer *text_layer;
 
-
 static void updateDisplay() {
   if(getEnablebackground()) {
     window_set_background_color(window, getBackground());
@@ -13,12 +12,14 @@ static void updateDisplay() {
   else {
     window_set_background_color(window,GColorWhite);
   }
+  switch(getFont_size()){
+    case FONT_SIZE_SMALL : text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14)); break;
+    case FONT_SIZE_NORMAL : text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD)); break;
+    case FONT_SIZE_LARGE : text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD)); break;
+  }
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
-  // Let Pebble Autoconfig handle received settings
-  enamel_in_received_handler(iter, context);
-
   updateDisplay();
 }
 
@@ -26,7 +27,7 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create(GRect(0, 62, bounds.size.w, 60));
+  text_layer = text_layer_create(bounds);
   text_layer_set_text(text_layer, "Change the background in the configuration page");
   text_layer_set_background_color(text_layer, GColorClear);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
@@ -40,11 +41,11 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
-  // Initialize Pebble Autoconfig to register App Message handlers and restores settings
+  // Initialize Enamel to register App Message handlers and restores settings
   enamel_init(0, 0);
 
-  // Register our custom receive handler which in turn will call Pebble Autoconfigs receive handler
-  app_message_register_inbox_received(in_received_handler);
+  // Register our custom receive handler
+  enamel_register_custom_inbox_received(in_received_handler);
 
   window = window_create();
   window_set_window_handlers(window, (WindowHandlers) {
@@ -56,8 +57,8 @@ static void init(void) {
 
 static void deinit(void) {
   window_destroy(window);
-  app_message_deregister_callbacks();
-
+  
+  // Deinit Enamel to unregister App Message handlers and save settings
   enamel_deinit();
 }
 
